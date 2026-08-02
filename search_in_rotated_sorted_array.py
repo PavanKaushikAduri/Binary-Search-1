5603197759e3352a2732fe9dd492b3c50cddfb38
Aduri Pavan Kaushik <apavankaushik@gmail.com>
2026-08-01 17:55:45 -0700

    Search in rotated sorted array in python

--- search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array.py
@@ -5,7 +5,7 @@
 
 
 # Your code here along with comments explaining your approach in three sentences only
-
+#working mechanism: we basically first find the sorted space and then we check if the target exists in the sorted space if it exists then we move the pointers to search for the target and return the index if found. if its not found we move the pointers to checkin other space. since the input array is rotated some part of the array is sorted and some part is unsorted, so we use this property to decide which part to search next.
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         low = 0
